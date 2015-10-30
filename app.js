5c803fefeaa0824edf9e63c574f156b494a21da7
Gabriel Poça <gabrielpoca@gmail.com>
2015-10-30 16:44:14 +0000

    Fix indentation and move constants into variables

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
-var express = require("express");
+var express = require('express');
 var BinaryServer = require('binaryjs').BinaryServer;
 var fs = require('fs');
 var wav = require('wav');
 
 var port = 3700;
+var outFile = 'demo.wav';
 var app = express();
+
 app.set('views', __dirname + '/tpl');
-app.set('view engine', "jade");
+app.set('view engine', 'jade');
 app.engine('jade', require('jade').__express);
 app.use(express.static(__dirname + '/public'))
 
-app.get("/", function(req, res){
-  res.render("index");
+app.get('/', function(req, res){
+  res.render('index');
 });
 
 app.listen(port);
 
+console.log('server open on port ' + port);
+
 binaryServer = BinaryServer({port: 9001});
 
 binaryServer.on('connection', function(client) {
-  console.log("new connection");
+  console.log('new connection');
 
-  var fileWriter = new wav.FileWriter('demo2.wav', {
+  var fileWriter = new wav.FileWriter(outFile, {
     channels: 1,
     sampleRate: 48000,
     bitDepth: 16
@@ -33,6 +37,7 @@ binaryServer.on('connection', function(client) {
 
     stream.on('end', function() {
       fileWriter.end();
+      console.log('wrote to file ' + outFile);
     });
   });
 });
