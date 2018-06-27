dea394dea427d62ba3f10f3b233f8be2fa0fbd46
antoineclaval <antoine.claval@gmail.com>
2018-06-26 23:40:18 -0500

    remove jade

--- app.js
+++ app.js
@@ -15,9 +15,7 @@ function guid() {
   return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
 }
 
-app.set('views', __dirname + '/tpl');
-app.set('view engine', 'jade');
-app.engine('jade', require('jade').__express);
+
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(req, res){
