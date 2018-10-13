957a471d0e4b51d44c33a736810f085a2a6cc6b8
James Wesley Goedert <jwgoedert@MacBook-Pro-10.local>
2018-10-13 13:31:44 -0500

    Add Icons and Styles

--- public/index.js
+++ public/index.js
@@ -1,11 +1,15 @@
 images = [1, 2, 3].map(() => `../assets/images/CraigD-Image-TopRight.jpg`);
   // images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
-const slideshowText = [
+  let upperSlidePosition= 0;
+  const slideshowText = [
     {value:1, display:'Global Good Morning is a project\b Collecting good mornings from\b around the world'},
     {value:2, display:'We hope to hear as many people,\b languages, time zones and voices\b as possible. When we get enough,\b we’ll make a sculpture that helps a\b viewer feel the turning of the\b earth. '},
     {value:3, display:'It doesn’t matter when you wake\b up or when your morning is. We\b just want to hear your voice, know\b the time and where you are in the\b world.'},
 ];
-
-const changeText=  () => {
+document.getElementById("slideText").innerHTML = slideshowText[upperSlidePosition].display;
+const changeText=  (pos) => {
+    upperSlidePosition = upperSlidePosition + pos;
+    console.log('pos', pos, slideshowText[upperSlidePosition].display);
+    document.getElementById("slideText").innerHTML = slideshowText[upperSlidePosition].display;
     console.log('changing some texty stuff');
 };
